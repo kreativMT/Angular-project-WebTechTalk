1f6bc3a031f41df317bfad76abd681e441a64e95
kreativMT <majzik.tibor@kreativszolnok.hu>
2023-09-01 22:03:11 +0200

    Data binding with NgModel OnInit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,6 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  username ="";
+  password ="";
+  errorMsg ="";
+
+  constructor( ) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    if(this.username.trim().length ===0) {
+      this.errorMsg ="Username is required"; 
+    }
+    else if(this.password.trim().length ===0) {
+      this.errorMsg ="Username is required"; 
+    }
+  }
 }
+
+
